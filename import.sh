afcfda677dbf49563ba8be0017016522270ad855
Alex Ough <alex.ough@sungardas.com>
2018-05-24 23:07:22 -0500

    changed to absolute path

--- import.sh
+++ import.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-source .import.env.local
+source /home/ec2-user/aws-services-billing/.import.env.local
 
 cd /home/ec2-user/aws-services-billing/src; source ~/.nvm/nvm.sh; nvm use 8; node run_import.js;
