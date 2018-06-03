b0a221ab15c1b6e38fb6c4146429fc7e23983d70
Alex Ough <alex.ough@sungardas.com>
2018-06-03 18:33:24 -0500

    removed unnecessary resources

--- buildspec.sh
+++ buildspec.sh
@@ -1,12 +1,6 @@
 
 source .env.local
 
-source .env.local
-
-sed -i 1 "s/AWS::REGION/$AWS_DEFAULT_REGION/g" swagger.yaml
-sed -i 2 "s/AWS::ACCOUNT_ID/$AWS_ACCOUNT_ID/g" swagger.yaml
-sed -i 2 "s/AWS::CUSTOM_AUTHORIZER_LAMBDA_NAME/$CUSTOM_AUTHORIZER_LAMBDA_NAME/g" swagger.yaml
-sed -i 2 "s/AWS::CUSTOM_AUTHORIZER_ROLE_NAME/$CUSTOM_AUTHORIZER_ROLE_NAME/g" swagger.yaml
 cd src; npm install; cd ..
 
 aws cloudformation package \
@@ -15,18 +9,12 @@ aws cloudformation package \
    --output-template-file samTemplate.yaml
 
 
-mv swagger.yaml1 swagger.yaml
-rm swagger.yaml2
-
-
 aws cloudformation deploy --template-file ./samTemplate.yaml \
   --capabilities CAPABILITY_IAM \
-  --stack-name SungardAS-aws-services-billing \
+  --stack-name SungardAS-aws-services-billing-us-east-2 \
   --parameter-overrides RedshiftUser=$REDSHIFT_USER RedshiftPass=$REDSHIFT_PASS \
   RedshiftDatabase=$REDSHIFT_DATABASE VpcCidr=$VPC_CIDR PublicCidr1=$PUBLIC_CIDR_1 PublicCidr2=$PUBLIC_CIDR_2 \
-  PrivateCidr1=$PRIVATE_CIDR_1 PrivateCidr2=$PRIVATE_CIDR_2 NameTag=$NAME_TAG AlarmThresholdNumber=$ALARM_THRESHOLD_NUMBER \
-  RedshiftSnapshotIdentifier=$REDSHIFT_SNAPSHOT_IDENTIFIER  RedshiftSnapshotClusterIdentifier=$REDSHIFT_SNAPSHOT_CLUSTER_IDENTIFIER \
-  AllowedAverageCost=$ALLOWED_AVERAGE_COST PlotlyUsername=$PLOTLY_USER_NAME PlotlyAPIKey=$PLOTLY_API_KEY \
-  BillingLogGroupName=$BILLING_LOG_GROUP_NAME SubscriptionFilterDestinationArn=$SUBSCRIPTION_FILTER_DESTINATION_ARN
+  PrivateCidr1=$PRIVATE_CIDR_1 PrivateCidr2=$PRIVATE_CIDR_2 NameTag=$NAME_TAG \
+  RedshiftSnapshotIdentifier=$REDSHIFT_SNAPSHOT_IDENTIFIER  RedshiftSnapshotClusterIdentifier=$REDSHIFT_SNAPSHOT_CLUSTER_IDENTIFIER
 
 rm samTemplate.yaml
--- import.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-source /home/ec2-user/aws-services-billing/.import.env.local
-
-cd /home/ec2-user/aws-services-billing/src; source ~/.nvm/nvm.sh; nvm use 8; node run_import.js;
